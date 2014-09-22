811aa88f9c734ad734a0a47ae4c58c755070da41
Dimitar Mitev <dnmitev@gmail.com>
2014-09-22 21:18:27 +0300

    exam praparation task is semi-done. no tests provided yet

---
+++ ExamPreparation/Articles.Wcf.Service/AlertService.svc.cs
@@ -0,0 +1,30 @@
+﻿namespace Articles.Wcf.Service
+{
+    using Articles.Data;
+    using Articles.Data.Contracts;
+    using Articles.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Runtime.Serialization;
+    using System.ServiceModel;
+    using System.ServiceModel.Activation;
+    using System.Text;
+
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class AlertService : IAlertService
+    {
+        private IAppData data;
+
+        public AlertService()
+        {
+            this.data = new AppData();
+        }
+
+        public ICollection<Alert> Get()
+        {
+            var alerts = this.data.Alerts.All().ToArray();
+            return alerts;
+        }
+    }
+}
---
+++ ExamPreparation/Articles.Wcf.Service/IAlertService.cs
@@ -0,0 +1,16 @@
+﻿namespace Articles.Wcf.Service
+{
+    using System.Collections.Generic;
+    using System.ServiceModel;
+    using System.ServiceModel.Web;
+    
+    using Articles.Models;
+
+    [ServiceContract]
+    public interface IAlertService
+    {
+        [OperationContract]
+        [WebGet(UriTemplate = "")]
+        ICollection<Alert> Get();
+    }
+}
